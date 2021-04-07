002888d2c122c76e5c3cba758c92ba18a621fdfe
Tomas McGuinness <tomas@tomasmcguinness.com>
2021-04-07 09:39:33 +0100

    Initial skeleton XCode app

---
+++ TestMDM/TestMDMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMDMApp.swift
+//  TestMDM
+//
+//  Created by Tomas McGuinness on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestMDMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

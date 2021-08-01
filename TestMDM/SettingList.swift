549a21c5a9e11f0f34086a19f2affaae764632b1
Tomas McGuinness <tomas@tomasmcguinness.com>
2021-08-01 08:39:01 +0100

    Delete icons as they aren't used. Add icons downloaded from new site

--- TestMDM/SettingList.swift
+++ TestMDM/SettingList.swift
@@ -11,10 +11,16 @@ struct SettingList: View {
     
     @ObservedObject var model = ManagedSettings()
     
+    init() {
+        let skyBlue = Color(red: 67/255, green: 56/255, blue: 202/255)
+
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: skyBlue]
+    }
+
     var body: some View {
         if(model.settings.count == 0) {
             VStack {
-                Text("There are no settings");
+                Text("There are no managed app settings")
             }
         } else {
             List(model.settings) {
